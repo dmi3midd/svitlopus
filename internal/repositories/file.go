f0f9fb129a654008392e880b487c5e4cd8941590
dmi3mid <dmi3midd@gmail.com>
2026-05-06 00:09:38 +0300

    feat: implement FileRepository and FolderRepository interfaces.

--- internal/repositories/file.go
+++ internal/repositories/file.go
@@ -2,7 +2,16 @@ package repositories
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"svitlopus/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	ErrFileNotFound = errors.New("file not found")
 )
 
 // FileRepository defines the interface for file repository operations.
@@ -14,10 +23,101 @@ type FileRepository interface {
 	// It returns an empty slice if no files are found with the given parent ID.
 	GetByParentId(ctx context.Context, parentId string) ([]models.File, error)
 	// Create adds a new file to the database.
-	Create(ctx context.Context, file *models.File) error
+	Create(ctx context.Context, file *models.File) (*models.File, error)
 	// Update modifies an existing file in the database.
 	// It returns the ErrFileNotFound error if the file does not exist.
-	Update(ctx context.Context, file *models.File) error
+	Update(ctx context.Context, file *models.File) (*models.File, error)
 	// Delete removes a file from the database by its ID.
 	Delete(ctx context.Context, id string) error
+}
+
+type fileRepository struct {
+	db *sqlx.DB
+}
+
+func NewFileRepo(db *sqlx.DB) FileRepository {
+	return &fileRepository{
+		db: db,
+	}
+}
+
+func (r *fileRepository) GetById(ctx context.Context, id string) (*models.File, error) {
+	op := "FileRepository.GetById"
+	query := `
+	SELECT id, title, mime, size, parent_id, file_id, message_id, created_at, updated_at 
+	FROM files 
+	WHERE id = $1
+	`
+	var file models.File
+	err := r.db.GetContext(ctx, &file, query, id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("%s: %w", op, ErrFileNotFound)
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return &file, nil
+}
+
+func (r *fileRepository) GetByParentId(ctx context.Context, parentId string) ([]models.File, error) {
+	op := "FileRepository.GetByParentId"
+	query := `
+	SELECT id, title, mime, size, parent_id, file_id, message_id, created_at, updated_at 
+	FROM files 
+	WHERE parent_id = $1
+	`
+	var files []models.File
+	err := r.db.SelectContext(ctx, &files, query, parentId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return files, nil
+}
+
+func (r *fileRepository) Create(ctx context.Context, file *models.File) (*models.File, error) {
+	op := "FileRepository.Create"
+	query := `
+	INSERT INTO files (id, title, mime, size, parent_id, file_id, message_id, created_at, updated_at) 
+	VALUES (:id, :title, :mime, :size, :parent_id, :file_id, :message_id, :created_at, :updated_at)
+	`
+	_, err := r.db.NamedExecContext(ctx, query, file)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return file, nil
+}
+
+func (r *fileRepository) Update(ctx context.Context, file *models.File) (*models.File, error) {
+	op := "FileRepository.Update"
+	query := `
+	UPDATE files 
+	SET title = :title, parent_id = :parent_id, updated_at = :updated_id
+	WHERE id = :id
+	`
+	result, err := r.db.NamedExecContext(ctx, query, file)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	if rowsAffected == 0 {
+		return nil, fmt.Errorf("%s: %w", op, ErrFileNotFound)
+	}
+	return file, nil
+}
+
+func (r *fileRepository) Delete(ctx context.Context, id string) error {
+	op := "FileRepository.Delete"
+	query := `
+	DELETE FROM files 
+	WHERE id = $1
+	`
+	_, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
--- internal/repositories/folder.go
+++ internal/repositories/folder.go
@@ -2,7 +2,16 @@ package repositories
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"svitlopus/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	ErrFolderNotFound = errors.New("folder not found")
 )
 
 // FolderRepository defines the interface for folder repository operations.
@@ -12,7 +21,7 @@ type FolderRepository interface {
 	GetById(ctx context.Context, id string) (*models.Folder, error)
 	// GetByParentId returns a list of folders by their parent ID.
 	// It returns an empty slice if no folders are found with the given parent ID.
-	GetByParentId(ctx context.Context, parrentId string) ([]models.Folder, error)
+	GetByParentId(ctx context.Context, parentId string) ([]models.Folder, error)
 	// Create adds a new folder to the database.
 	Create(ctx context.Context, folder *models.Folder) (*models.Folder, error)
 	// Update modifies an existing folder in the database.
@@ -20,4 +29,93 @@ type FolderRepository interface {
 	Update(ctx context.Context, folder *models.Folder) (*models.Folder, error)
 	// Delete removes a folder from the database by its ID.
 	Delete(ctx context.Context, id string) error
+}
+
+type folderRepository struct {
+	db *sqlx.DB
+}
+
+func NewFolderRepo(db *sqlx.DB) FolderRepository {
+	return &folderRepository{
+		db: db,
+	}
+}
+
+func (r *folderRepository) GetById(ctx context.Context, id string) (*models.Folder, error) {
+	op := "FolderRepository.GetById"
+	query := `
+	SELECT id, title, parent_id, created_at, updated_at 
+	FROM folders 
+	WHERE id = $1
+	`
+	var folder models.Folder
+	err := r.db.GetContext(ctx, &folder, query, id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("%s: %w", op, ErrFolderNotFound)
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return &folder, nil
+}
+
+func (r *folderRepository) GetByParentId(ctx context.Context, parentId string) ([]models.Folder, error) {
+	op := "FolderRepository.GetByParentId"
+	query := `
+	SELECT id, title, parent_id, created_at, updated_at 
+	FROM folders 
+	WHERE parent_id = $1
+	`
+	var folders []models.Folder
+	err := r.db.SelectContext(ctx, &folders, query, parentId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return folders, nil
+}
+
+func (r *folderRepository) Create(ctx context.Context, folder *models.Folder) (*models.Folder, error) {
+	op := "FolderRepository.Create"
+	query := `
+	INSERT INTO folders (id, title, parent_id, created_at, updated_at) 
+	VALUES (:id, :title, :parent_id, :created_at, :updated_at)
+	`
+	if _, err := r.db.NamedExecContext(ctx, query, folder); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return folder, nil
+}
+
+func (r *folderRepository) Update(ctx context.Context, folder *models.Folder) (*models.Folder, error) {
+	op := "FolderRepository.Update"
+	query := `
+	UPDATE folders 
+	SET title = :title, parent_id = :parent_id, updated_at = :updated_at, created_at = :created_at
+	WHERE id = :id
+	`
+	result, err := r.db.NamedExecContext(ctx, query, folder)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	if rowsAffected == 0 {
+		return nil, fmt.Errorf("%s: %w", op, ErrFolderNotFound)
+	}
+	return folder, nil
+}
+
+func (r *folderRepository) Delete(ctx context.Context, id string) error {
+	op := "FolderRepository.Delete"
+	query := `
+	DELETE FROM folders 
+	WHERE id = $1
+	`
+	_, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
